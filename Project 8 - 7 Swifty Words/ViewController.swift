4b6a8d73e0b434a588b76eb190a14fa361fd87df
Igor Polousov <igorpolousov@gmail.com>
2021-07-23 12:32:25 +0300

    + necessary vars

--- Project 8 - 7 Swifty Words/ViewController.swift
+++ Project 8 - 7 Swifty Words/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentUnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
